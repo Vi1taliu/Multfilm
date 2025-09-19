76b7f3cb699bd6edb3e5ca51bbd094f5b5f333f6
Vi1taliu <vitalok2010@gmail.com>
2025-09-19 18:32:28 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,16 +2,53 @@
 
 void drawHangar()
 {
-    txSetColor (RGB(61, 54, 54));
-    txSetFillColor (RGB(61, 54, 54));
+    txSetColor (RGB(100, 100, 100));
+    txSetFillColor (RGB(100, 100, 100));
     txRectangle (0, 0, 800, 600);
+
+    txSetColor (RGB(45, 191, 196));
+    txSetFillColor (RGB(45, 191, 196));
+    txRectangle (97, 80, 698, 171);
+
+    txSetColor (RGB(0, 0, 0));
+    txSetFillColor (RGB(0, 0, 0));
+    txRectangle (34, 0, 49, 600);
+
+    txSetColor (RGB(0, 0, 0));
+    txSetFillColor (RGB(0, 0, 0));
+    txRectangle (766, 0, 751, 600);
+}
+
+void drawTank(int x)
+{
+    txSetColor (RGB(13, 125, 13));
+    txSetFillColor (RGB(13, 125, 13));
+    POINT tank[4] = {{197, 360}, {603, 360}, {659, 455}, {126, 455}};
+    txPolygon (tank, 4);
+
+    txSetColor (RGB(13, 125, 13));
+    txSetFillColor (RGB(13, 125, 13));
+    POINT tank2[4] = {{367, 234}, {554, 234}, {554, 348}, {367, 348}};
+    txPolygon (tank2, 4);
+
 }
 
 int main()
-    {
+{
     txCreateWindow (800, 600);
 
-    txTextCursor (false);
-    return 0;
+    //int xTank = 100;
+
+    //while (xTank<347)
+    {
+        txBegin();
+        drawHangar();
+        drawTank();
+        txEnd();
+        //xTank += 1;
     }
+
+txTextCursor (false);
+return 0;
+}
 
